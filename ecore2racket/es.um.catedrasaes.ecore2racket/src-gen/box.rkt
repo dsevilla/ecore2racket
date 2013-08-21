365224973d9c5da58af234ac6905ce1fa11038f3
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2013-08-22 01:09:49 +0200

    Added a minimal ecore support, and continue growing the generator.

--- ecore2racket/es.um.catedrasaes.ecore2racket/src-gen/box.rkt
+++ ecore2racket/es.um.catedrasaes.ecore2racket/src-gen/box.rkt
@@ -4,6 +4,9 @@
 
   (require racket/class)
 
+  ;; Support functions
+  (require "ecore-support.rkt")
+
   (provide
   		box%
   		box-reference%
@@ -23,32 +26,83 @@
 
 ;; Class Box (box%)
 (define box%
-  (class object%
-  
+  (class* eobject%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"Box")
+	(define/override (e-attributes)
+		'( 
+		comment
+		name
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		attributes
+		methods
+		wires
+		boxes
+		connection
+		)
+	)
+
 
     ;; fields (features)
 	; attribute comment
-	(field (comment 0))
+	(define -comment 0)
+	(define/public (comment)
+		-comment)
+	(define/public (comment-set! val)
+		(set! -comment val))
 	
 	; attribute name
-	(field (name 0))
+	(define -name 0)
+	(define/public (name)
+		-name)
+	(define/public (name-set! val)
+		(set! -name val))
 	
 	
 	; reference attributes
-	(field (attributes null))
+	(define -attributes null)
+	(define/public (attributes)
+		-attributes)
+	(define/public (attributes-set! val)
+		(set! -attributes val))
 	
 	; reference methods
-	(field (methods null))
+	(define -methods null)
+	(define/public (methods)
+		-methods)
+	(define/public (methods-set! val)
+		(set! -methods val))
 	
 	; reference wires
-	(field (wires null))
+	(define -wires null)
+	(define/public (wires)
+		-wires)
+	(define/public (wires-set! val)
+		(set! -wires val))
 	
 	; reference boxes
-	(field (boxes null))
+	(define -boxes null)
+	(define/public (boxes)
+		-boxes)
+	(define/public (boxes-set! val)
+		(set! -boxes val))
 	
 	; reference connection
-	(field (connection null))
+	(define -connection null)
+	(define/public (connection)
+		-connection)
+	(define/public (connection-set! val)
+		(set! -connection val))
 	
 	
 
@@ -57,23 +111,59 @@
 
 ;; Class BoxReference (box-reference%)
 (define box-reference%
-  (class object%
-  
+  (class* eobject%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"BoxReference")
+	(define/override (e-attributes)
+		'( 
+		is-pointer
+		is-reference
+		name
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		box
+		)
+	)
+
 
     ;; fields (features)
 	; attribute is-pointer
-	(field (is-pointer 0))
+	(define -is-pointer 0)
+	(define/public (is-pointer)
+		-is-pointer)
+	(define/public (is-pointer-set! val)
+		(set! -is-pointer val))
 	
 	; attribute is-reference
-	(field (is-reference 0))
+	(define -is-reference 0)
+	(define/public (is-reference)
+		-is-reference)
+	(define/public (is-reference-set! val)
+		(set! -is-reference val))
 	
 	; attribute name
-	(field (name 0))
+	(define -name 0)
+	(define/public (name)
+		-name)
+	(define/public (name-set! val)
+		(set! -name val))
 	
 	
 	; reference box
-	(field (box null))
+	(define -box null)
+	(define/public (box)
+		-box)
+	(define/public (box-set! val)
+		(set! -box val))
 	
 	
 
@@ -82,14 +172,35 @@
 
 ;; Class WireReference (wire-reference%)
 (define wire-reference%
-  (class object%
-  
+  (class* eobject%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"WireReference")
+	(define/override (e-attributes)
+		'( 
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		wire
+		)
+	)
+
 
     ;; fields (features)
 	
 	; reference wire
-	(field (wire null))
+	(define -wire null)
+	(define/public (wire)
+		-wire)
+	(define/public (wire-set! val)
+		(set! -wire val))
 	
 	
 
@@ -98,9 +209,25 @@
 
 ;; Class LocalWireReference (local-wire-reference%)
 (define local-wire-reference%
-  (class wire-reference%
-  
+  (class* wire-reference%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"LocalWireReference")
+	(define/override (e-attributes)
+		'( 
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		)
+	)
+
 
     ;; fields (features)
 	
@@ -111,14 +238,35 @@
 
 ;; Class ExternalWireReference (external-wire-reference%)
 (define external-wire-reference%
-  (class wire-reference%
-  
+  (class* wire-reference%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"ExternalWireReference")
+	(define/override (e-attributes)
+		'( 
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		box-reference
+		)
+	)
+
 
     ;; fields (features)
 	
 	; reference box-reference
-	(field (box-reference null))
+	(define -box-reference null)
+	(define/public (box-reference)
+		-box-reference)
+	(define/public (box-reference-set! val)
+		(set! -box-reference val))
 	
 	
 
@@ -127,17 +275,43 @@
 
 ;; Class Connection (connection%)
 (define connection%
-  (class object%
-  
+  (class* eobject%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"Connection")
+	(define/override (e-attributes)
+		'( 
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		from
+		to
+		)
+	)
+
 
     ;; fields (features)
 	
 	; reference from
-	(field (from null))
+	(define -from null)
+	(define/public (from)
+		-from)
+	(define/public (from-set! val)
+		(set! -from val))
 	
 	; reference to
-	(field (to null))
+	(define -to null)
+	(define/public (to)
+		-to)
+	(define/public (to-set! val)
+		(set! -to val))
 	
 	
 
@@ -146,20 +320,51 @@
 
 ;; Class Wire (wire%)
 (define wire%
-  (class object%
-  
+  (class* eobject%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"Wire")
+	(define/override (e-attributes)
+		'( 
+		is-input
+		name
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		type
+		)
+	)
+
 
     ;; fields (features)
 	; attribute is-input
-	(field (is-input 0))
+	(define -is-input 0)
+	(define/public (is-input)
+		-is-input)
+	(define/public (is-input-set! val)
+		(set! -is-input val))
 	
 	; attribute name
-	(field (name 0))
+	(define -name 0)
+	(define/public (name)
+		-name)
+	(define/public (name-set! val)
+		(set! -name val))
 	
 	
 	; reference type
-	(field (type null))
+	(define -type null)
+	(define/public (type)
+		-type)
+	(define/public (type-set! val)
+		(set! -type val))
 	
 	
 
@@ -168,16 +373,42 @@
 
 ;; Class Type (type%)
 (define type%
-  (class object%
-  
+  (class* eobject%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"Type")
+	(define/override (e-attributes)
+		'( 
+		is-pointer
+		is-reference
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		)
+	)
+
 
     ;; fields (features)
 	; attribute is-pointer
-	(field (is-pointer 0))
+	(define -is-pointer 0)
+	(define/public (is-pointer)
+		-is-pointer)
+	(define/public (is-pointer-set! val)
+		(set! -is-pointer val))
 	
 	; attribute is-reference
-	(field (is-reference 0))
+	(define -is-reference 0)
+	(define/public (is-reference)
+		-is-reference)
+	(define/public (is-reference-set! val)
+		(set! -is-reference val))
 	
 	
 	
@@ -187,13 +418,34 @@
 
 ;; Class BasicType (basic-type%)
 (define basic-type%
-  (class type%
-  
+  (class* type%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"BasicType")
+	(define/override (e-attributes)
+		'( 
+		basic-type
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		)
+	)
+
 
     ;; fields (features)
 	; attribute basic-type
-	(field (basic-type 0))
+	(define -basic-type 0)
+	(define/public (basic-type)
+		-basic-type)
+	(define/public (basic-type-set! val)
+		(set! -basic-type val))
 	
 	
 	
@@ -203,16 +455,42 @@
 
 ;; Class OtherType (other-type%)
 (define other-type%
-  (class type%
-  
+  (class* type%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"OtherType")
+	(define/override (e-attributes)
+		'( 
+		namespace
+		name
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		)
+	)
+
 
     ;; fields (features)
 	; attribute namespace
-	(field (namespace 0))
+	(define -namespace 0)
+	(define/public (namespace)
+		-namespace)
+	(define/public (namespace-set! val)
+		(set! -namespace val))
 	
 	; attribute name
-	(field (name 0))
+	(define -name 0)
+	(define/public (name)
+		-name)
+	(define/public (name-set! val)
+		(set! -name val))
 	
 	
 	
@@ -222,26 +500,67 @@
 
 ;; Class Method (method%)
 (define method%
-  (class object%
-  
+  (class* eobject%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"Method")
+	(define/override (e-attributes)
+		'( 
+		comment
+		is-private
+		name
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		type
+		parameters
+		)
+	)
+
 
     ;; fields (features)
 	; attribute comment
-	(field (comment 0))
+	(define -comment 0)
+	(define/public (comment)
+		-comment)
+	(define/public (comment-set! val)
+		(set! -comment val))
 	
 	; attribute is-private
-	(field (is-private 0))
+	(define -is-private 0)
+	(define/public (is-private)
+		-is-private)
+	(define/public (is-private-set! val)
+		(set! -is-private val))
 	
 	; attribute name
-	(field (name 0))
+	(define -name 0)
+	(define/public (name)
+		-name)
+	(define/public (name-set! val)
+		(set! -name val))
 	
 	
 	; reference type
-	(field (type null))
+	(define -type null)
+	(define/public (type)
+		-type)
+	(define/public (type-set! val)
+		(set! -type val))
 	
 	; reference parameters
-	(field (parameters null))
+	(define -parameters null)
+	(define/public (parameters)
+		-parameters)
+	(define/public (parameters-set! val)
+		(set! -parameters val))
 	
 	
 
@@ -250,23 +569,59 @@
 
 ;; Class Param (param%)
 (define param%
-  (class object%
-  
+  (class* eobject%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"Param")
+	(define/override (e-attributes)
+		'( 
+		comment
+		name
+		default-value
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		type
+		)
+	)
+
 
     ;; fields (features)
 	; attribute comment
-	(field (comment 0))
+	(define -comment 0)
+	(define/public (comment)
+		-comment)
+	(define/public (comment-set! val)
+		(set! -comment val))
 	
 	; attribute name
-	(field (name 0))
+	(define -name 0)
+	(define/public (name)
+		-name)
+	(define/public (name-set! val)
+		(set! -name val))
 	
 	; attribute default-value
-	(field (default-value 0))
+	(define -default-value 0)
+	(define/public (default-value)
+		-default-value)
+	(define/public (default-value-set! val)
+		(set! -default-value val))
 	
 	
 	; reference type
-	(field (type null))
+	(define -type null)
+	(define/public (type)
+		-type)
+	(define/public (type-set! val)
+		(set! -type val))
 	
 	
 
@@ -275,9 +630,25 @@
 
 ;; Class WireMany (wire-many%)
 (define wire-many%
-  (class wire%
-  
+  (class* wire%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"WireMany")
+	(define/override (e-attributes)
+		'( 
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		)
+	)
+
 
     ;; fields (features)
 	
@@ -288,14 +659,35 @@
 
 ;; Class WireMethod (wire-method%)
 (define wire-method%
-  (class wire%
-  
+  (class* wire%
+   (classifier&)
     (super-new)
+
+	;; Make it available for introspection (TODO: see a more general method)
+	(inspect #f)
+
+	;; classifier& interface methods
+    (define/override (e-name)
+		"WireMethod")
+	(define/override (e-attributes)
+		'( 
+		)	
+	)
+	(define/override (e-references)
+		'( 
+		method
+		)
+	)
+
 
     ;; fields (features)
 	
 	; reference method
-	(field (method null))
+	(define -method null)
+	(define/public (method)
+		-method)
+	(define/public (method-set! val)
+		(set! -method val))
 	
 	
 
---
+++ ecore2racket/es.um.catedrasaes.ecore2racket/src/ecore-support.rkt
@@ -0,0 +1,37 @@
+#lang racket/base
+
+;; Minimal interface that resembles ecore metadata
+;; As ecore models representational elements are in turn classes
+;; and instances, I only include here the static typing information
+;; needed to know how to treat each feature of a class (for example,
+;; the differentiation between attributes and references even when
+;; they're represented by the same abstraction (private field + get &
+;; set functions).
+
+(require racket/class)
+
+(provide
+   named-element&
+   classifier&
+   structural-feature&
+   reference&
+   attribute&
+   eobject%)
+
+(define named-element& (interface () e-name))
+(define classifier& (interface (named-element&)
+                      ;; superclass
+                      e-attributes e-references))
+(define structural-feature& (interface (named-element&) e-type))
+(define reference& (interface (structural-feature&)))
+(define attribute& (interface (structural-feature&)))
+
+
+(define eobject%
+  (class* object% (classifier&)
+    ;; classifier& interface methods
+    (define/public (e-name)
+      "EObject")
+    (define/public (e-attributes) null)
+    (define/public (e-references) null)
+    ))
