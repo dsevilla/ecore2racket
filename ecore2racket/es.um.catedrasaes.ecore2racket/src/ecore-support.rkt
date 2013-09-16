332eaa5ab91decfef9bd2d89d2858ffd6fd33f1d
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2013-09-16 21:54:35 +0200

    misc. changes.

--- ecore2racket/es.um.catedrasaes.ecore2racket/src/ecore-support.rkt
+++ ecore2racket/es.um.catedrasaes.ecore2racket/src/ecore-support.rkt
@@ -45,10 +45,15 @@
 
     (define/public (e-package) -e-package)
     (define/public (e-package-set! p) (set! -e-package p))
-    (define/public (e-attributes) null)
-    (define/public (e-references) null)
-    (define/public (e-all-attributes) null)
-    (define/public (e-all-references) null)))
+    
+    (field [-e-attributes null]
+           [-e-references null]
+           [-e-all-attributes null]
+           [-e-all-references null])
+    (define/public (e-attributes) -e-attributes)
+    (define/public (e-references) -e-references)
+    (define/public (e-all-attributes) -e-all-attributes)
+    (define/public (e-all-references) -e-all-references)))
 
 (define epackage%
   (class* eobject% ()
@@ -112,49 +117,75 @@
   (define (expand-class-attribute list)
     (match list
       ((list 'attribute name type minoccur maxoccur)
-       (let ((private-name (append-id "-" name))
+       (let ((field-name (append-id "-" name))
              (set-name (append-id name "-set!")))
          (if (= maxoccur 1)
-             `((define ,private-name 0)
-               (define/public (,name) ,private-name)
+             `((field [,field-name 0])
+               (define/public (,name) ,field-name)
                (define/public (,set-name value)
-                 (set! ,private-name value)))
+                 (set! ,field-name value)))
              ;; multi-valuated
              (with-gensyms (tmp-vec-n tmp-pos-n tmp-val-n)
-               `((define ,private-name (make-vector 0))
+               `((field [,field-name (make-vector 0)])
                  (define ,name
                    (case-lambda
-                     (() ,private-name)
+                     (() ,field-name)
                      ((,tmp-pos-n)
-                      (when (<= (vector-length ,private-name) ,tmp-pos-n)
+                      (when (<= (vector-length ,field-name) ,tmp-pos-n)
                         (let ((,tmp-vec-n (make-vector (add1 ,tmp-pos-n) null)))
                           ;; grow the vector
-                          (vector-copy! ,tmp-vec-n 0 ,private-name)
-                          (set! ,private-name ,tmp-vec-n)))
-                      (vector-ref ,private-name ,tmp-pos-n))))
+                          (vector-copy! ,tmp-vec-n 0 ,field-name)
+                          (set! ,field-name ,tmp-vec-n)))
+                      (vector-ref ,field-name ,tmp-pos-n))))
                  (public ,name)
                  (define ,set-name
                    (case-lambda
-                     ((,tmp-val-n) (set! ,private-name ,tmp-val-n))
+                     ((,tmp-val-n) (set! ,field-name ,tmp-val-n))
                      ((,tmp-val-n ,tmp-pos-n)
-                      (when (<= (vector-length ,private-name) ,tmp-pos-n)
+                      (when (<= (vector-length ,field-name) ,tmp-pos-n)
                         (let ((,tmp-vec-n (make-vector (add1 ,tmp-pos-n) null)))
                           ;; grow the vector
-                          (vector-copy! ,tmp-vec-n 0 ,private-name)
-                          (set! ,private-name ,tmp-vec-n)))
-                      (vector-set! ,private-name ,tmp-pos-n ,tmp-val-n))))
+                          (vector-copy! ,tmp-vec-n 0 ,field-name)
+                          (set! ,field-name ,tmp-vec-n)))
+                      (vector-set! ,field-name ,tmp-pos-n ,tmp-val-n))))
                  (public ,set-name))))))))
 
   (define (expand-class-reference list)
     (match list
-      ((list 'attribute name type minoccur maxoccur)
-       (let ((private-name (append-id "-" name))
+      ((list 'reference name type minoccur maxoccur)
+       (let ((field-name (append-id "-" name))
              (set-name (append-id name "-set!")))
-         `((define ,private-name 0)
-           (define/public (,name) ,private-name)
-           (define/public (,set-name value)
-             (set! ,private-name value)))))))
-
+         (if (= maxoccur 1)
+             `((field [,field-name null])
+               (define/public (,name) ,field-name)
+               (define/public (,set-name value)
+                 (set! ,field-name value)))
+             ;; multi-valuated
+             (with-gensyms
+              (tmp-vec-n tmp-pos-n tmp-val-n)
+              `((field [,field-name (make-vector 0 null)])
+                (define ,name
+                  (case-lambda
+                    (() ,field-name)
+                    ((,tmp-pos-n)
+                     (when (<= (vector-length ,field-name) ,tmp-pos-n)
+                       (let ((,tmp-vec-n (make-vector (add1 ,tmp-pos-n) null)))
+                         ;; grow the vector
+                         (vector-copy! ,tmp-vec-n 0 ,field-name)
+                         (set! ,field-name ,tmp-vec-n)))
+                     (vector-ref ,field-name ,tmp-pos-n))))
+                 (public ,name)
+                 (define ,set-name
+                   (case-lambda
+                     ((,tmp-val-n) (set! ,field-name ,tmp-val-n))
+                     ((,tmp-val-n ,tmp-pos-n)
+                      (when (<= (vector-length ,field-name) ,tmp-pos-n)
+                        (let ((,tmp-vec-n (make-vector (add1 ,tmp-pos-n) null)))
+                          ;; grow the vector
+                          (vector-copy! ,tmp-vec-n 0 ,field-name)
+                          (set! ,field-name ,tmp-vec-n)))
+                      (vector-set! ,field-name ,tmp-pos-n ,tmp-val-n))))
+                 (public ,set-name))))))))
 
   (define (expand-eclass-body body)
     (if (null? body)
@@ -172,7 +203,6 @@
                     (expand-eclass-body (cdr body)))
             (cons (car body) (expand-eclass-body (cdr body))))))
 
-
   )
 
 ;; The macro proper.
@@ -180,14 +210,24 @@
   `(define ,n
      (class ,super
        (super-new)
-       (define/public (attributes) #[,@(filter-by-application-symbol 'attribute body)])
-       (define/public (references) #[,@(filter-by-application-symbol 'reference body)])
+       
+       (inherit-field -e-name -e-package)
+       (set! -e-name ,(symbol->string n))
+       (set! -e-package null)
+       
+       ;; Normal class fields
+       (field [-e-attributes #[,@(filter-by-application-symbol 'attribute body)]])
+       (define (e-attributes) -e-attributes)
+       (field [-e-references #[,@(filter-by-application-symbol 'reference body)]])
+       (define (e-references) -e-references)
+       
        ,@(expand-eclass-body body))))
 
 
 ;;; test
-(eclass x% object%
+(eclass x% eobject%
        (define/public (test1) 1)
        (attribute pepe 'string 1 10)
        (attribute juan 'string 1 1)
+       (reference abc eobject% 1 1)
        (define/public (test2) 2))
